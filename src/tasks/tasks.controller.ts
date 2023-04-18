79eb34a8ad4a62a2ddfdab7c6513576d73a52640
Brittany Smith <Britain.D.Smith@gmail.com>
2023-04-18 12:12:22 -0500

    adding createTask route, controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { title } from 'process';
+
 
 @Controller('tasks')
 export class TasksController {
@@ -10,4 +12,12 @@ export class TasksController {
     getAllTasks(): Task[]{
         return this.tasksService.getAllTasks();
     }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        return this.tasksService.createTask(title, description);
+    }   
 }
